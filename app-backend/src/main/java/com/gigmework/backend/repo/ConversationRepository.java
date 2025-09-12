4fbb18fdccb47b2589c972519cb65c157afa0115
slutukurthi_expedia <slutukurthi@expediagroup.com>
2025-09-13 00:56:50 +0530

    Minor STABLE -33:  Deleted unwanted files

--- app-backend/src/main/java/com/gigmework/backend/repo/ConversationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gigmework.backend.repo;
-
-import com.gigmework.backend.domain.Conversation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConversationRepository extends JpaRepository<Conversation, Long> {
-}
-
